d68424d35ae799606e56d4bdb54875f7ffa8776f
Tiago Scolari <tscolari@gmail.com>
2016-01-13 23:58:27 +0000

    Add provisioning endpoint

--- design/broker.go
+++ design/broker.go
@@ -24,3 +24,23 @@ var _ = Resource("Catalog", func() {
 		Response(OK)
 	})
 })
+
+var _ = Resource("Provisioning", func() {
+	BasePath("/service_instances")
+
+	Action("create", func() {
+		Routing(GET("/:instance_id"))
+		Description("Provision a new service instance")
+		Params(func() {
+			Param("instance_id", String, "id of the service being provisioned")
+			Param("organization_guid", String, "The Cloud Controller GUID of the organization under which the service is to be provisioned")
+			Param("plan_id", String, "The ID of the plan that the user would like provisioned")
+			Param("service_id", String, "The ID of the service within the catalog")
+			Param("space_guid", String, "Similar to organization_guid, but for the space")
+			Param("accepts_incomplete", Boolean, "A value of true indicates that both the Cloud Controller and the requesting client support asynchronous provisioning")
+		})
+		Response(ServiceUnavailable)
+		Response(Created)
+		Response(Conflict)
+	})
+})
---
+++ design/provision_media.go
@@ -0,0 +1,13 @@
+package design
+
+// import (
+// 	. "github.com/raphael/goa/design/dsl"
+// )
+
+// var ProvisionMedia = MediaType("application/cfbroker.provision+json", func() {
+// 	Description("Service Provision")
+
+// 	Attributes(func() {
+// 		View("default", func() {})
+// 	})
+// })
